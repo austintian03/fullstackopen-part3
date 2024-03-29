266e3dd08bc30e2fb8cd8c02cf9d7024487ba14d
Austin Tian <adt339@nyu.edu>
2024-03-29 11:16:49 -0400

    3.2: add /info route to application

--- index.js
+++ index.js
@@ -2,33 +2,40 @@ const express = require('express')
 const app = express()
 
 let phonebook = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
 ]
 
+app.get('/info', (req, res) => {
+  const timestamp = `<p>${Date()}</p>`
+  const info = `<p>Phonebook has info for ${phonebook.length} people</p>`
+
+  res.send(info.concat(" ", timestamp))
+})
+
 app.get('/api/persons', (req, res) => {
-    res.json(phonebook)
+  res.json(phonebook)
 })
 
 const PORT = 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
