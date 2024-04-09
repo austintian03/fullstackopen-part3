1150361d0caedf20078ae47e3b144cc71b8f83fb
Austin Tian <adt339@nyu.edu>
2024-04-08 22:03:18 -0400

    3.13 - 3.14: integrate mongoose into backend to fetch and save data to database

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const Person = require('./models/person')
 
 morgan.token('data', (req, res) => {
   const data = JSON.stringify(req.body)
@@ -10,29 +12,6 @@ morgan.token('data', (req, res) => {
 })
 
 const app = express()
-
-let phonebook = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
 
 app.use(express.json())
 app.use(express.static('dist'))
@@ -46,12 +25,10 @@ app.get('/info', (req, res) => {
 })
 
 app.get('/api/persons', (req, res) => {
-  res.json(phonebook)
+  Person.find({}).then(people => {
+    res.json(people)
+  })
 })
-
-const generateId = () => {
-  return Math.floor(Math.random() * 10000)
-}
 
 app.post('/api/persons', (req, res) => {
   const body = req.body
@@ -60,18 +37,20 @@ app.post('/api/persons', (req, res) => {
     return res.status(400).json({ error: 'name or number is missing, please provide both' })
   }
 
+  /*
   if(phonebook.find(person => person.name === body.name)) {
     return res.status(400).json({ error: 'name must be unique' })
   }
-
-  const person = {
+  */
+ 
+  const person = new Person({
     "name": body.name,
     "number": body.number,
-    "id": generateId()
-  }
+  })
 
-  phonebook = phonebook.concat(person)
-  res.json(person)
+  person.save().then(savedPerson => {
+    res.json(savedPerson)
+  })
 })
 
 app.get('/api/persons/:id', (req, res) => {
@@ -97,7 +76,7 @@ app.delete('/api/persons/:id', (req, res) => {
   }
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
