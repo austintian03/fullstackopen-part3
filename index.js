53137cf9343317e528160506a7d7d90dc7c86354
Austin Tian <adt339@nyu.edu>
2024-04-09 13:46:33 -0400

    3.18: updating handling of the /api/persons/:id and /info routes to use the database

--- index.js
+++ index.js
@@ -17,17 +17,22 @@ app.use(express.json())
 app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
-app.get('/info', (req, res) => {
-  const timestamp = `<p>${Date()}</p>`
-  const info = `<p>Phonebook has info for ${phonebook.length} people</p>`
+app.get('/info', (req, res, next) => {
+  Person.countDocuments()
+    .then(count => {
+      const timestamp = `<p>${Date()}</p>`
+      const info = `<p>Phonebook has info for ${count} people</p>`
 
-  res.send(info.concat(" ", timestamp))
+      res.send(info.concat(" ", timestamp))
+    })
+    .catch(error => next(error))
 })
 
 app.get('/api/persons', (req, res) => {
-  Person.find({}).then(people => {
-    res.json(people)
-  })
+  Person.find({})
+    .then(people => {
+      res.json(people)
+    })
 })
 
 app.post('/api/persons', (req, res) => {
@@ -36,12 +41,6 @@ app.post('/api/persons', (req, res) => {
   if(!body.name || !body.number) {
     return res.status(400).json({ error: 'name or number is missing, please provide both' })
   }
-
-  /*
-  if(phonebook.find(person => person.name === body.name)) {
-    return res.status(400).json({ error: 'name must be unique' })
-  }
-  */
 
   const person = new Person({
     "name": body.name,
@@ -53,15 +52,16 @@ app.post('/api/persons', (req, res) => {
   })
 })
 
-app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = phonebook.find(person => person.id === id)
-
-  if (person) {
-    res.json(person)
-  } else {
-    res.status(404).send({ error: 'That person is not in the phonebook.'})
-  }
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (req, res) => {
@@ -72,7 +72,7 @@ app.put('/api/persons/:id', (req, res) => {
     number: body.number,
   }
 
-  Person.findById(req.params.id, person, { new: true })
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
     .then(updatedPerson => {
       res.json(updatedPerson)
     })
